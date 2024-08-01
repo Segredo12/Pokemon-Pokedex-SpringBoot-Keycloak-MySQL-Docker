2dd73a3473d270e2c61b617b3a14b36c2ecfde0a
Octavio Marques <octavio.marques@nearsoft.pt>
2024-08-01 10:59:53 +0100

    init api project

---
+++ pokemon-pokedex/src/main/java/com/segredo/pokemon_pokedex/PokemonPokedexApplication.java
@@ -0,0 +1,13 @@
+package com.segredo.pokemon_pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonPokedexApplication.class, args);
+	}
+
+}
---
+++ pokemon-pokedex/src/test/java/com/segredo/pokemon_pokedex/PokemonPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segredo.pokemon_pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonPokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
